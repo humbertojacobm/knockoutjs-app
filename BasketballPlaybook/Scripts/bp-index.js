dc696282394261feb8c127cc822856b70fb232c1
humberto mori <hmori@PRXL-HMORI-7400.atsol.com>
2020-03-04 18:35:12 -0500

    bindings: nested observables-> $root , $parent, tecnique to create nested observables

--- BasketballPlaybook/Scripts/bp-index.js
+++ BasketballPlaybook/Scripts/bp-index.js
@@ -8,16 +8,34 @@
 $(function() {
     $("#tagDialog").hide();
 
+    // var data = [
+    //     { Id: 1, Name: "Ball Handling" },
+    //     { Id: 2, Name: "Passing" },
+    //     { Id: 3, Name: "Shooting" },
+    //     { Id: 4, Name: "Rebounding" },
+    //     { Id: 5, Name: "Transition" },
+    //     { Id: 6, Name: "Defense" },
+    //     { Id: 7, Name: "Team Offense" },
+    //     { Id: 8, Name: "Team Defense" }
+    // ];
+
     var data = [
-        { Id: 1, Name: "Ball Handling" },
-        { Id: 2, Name: "Passing" },
-        { Id: 3, Name: "Shooting" },
-        { Id: 4, Name: "Rebounding" },
-        { Id: 5, Name: "Transition" },
-        { Id: 6, Name: "Defense" },
-        { Id: 7, Name: "Team Offense" },
-        { Id: 8, Name: "Team Defense" }
-    ];
+       new tagItem("Ball Handling", 1),
+       new tagItem("Passing", 2),
+       new tagItem("Shooting", 3),
+       new tagItem("Rebounding", 4),
+       new tagItem("Transition", 5),
+       new tagItem("Defense", 6),
+       new tagItem("Team Offense", 7),
+       new tagItem("Team Defense", 8)
+   ];
+
+    function tagItem(name, id){
+        return {
+            Name: ko.observable(name),
+            Id: ko.observable(id)
+        }
+    }
 
     var viewModel = {
         //data
