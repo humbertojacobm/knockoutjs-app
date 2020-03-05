8e8006d697d20b98cc3932198602536b23d0590b
humberto mori <hmori@PRXL-HMORI-7400.atsol.com>
2020-03-05 18:06:02 -0500

    data features: knockoututilities-> protected observables,commit, reset, doing this with a basic code

--- BasketballPlaybook/Scripts/bp-index.js
+++ BasketballPlaybook/Scripts/bp-index.js
@@ -8,38 +8,38 @@
 $(function() {
     $("#tagDialog").hide();
 
-    // var data = [
-    //     { Id: 1, Name: "Ball Handling" },
-    //     { Id: 2, Name: "Passing" },
-    //     { Id: 3, Name: "Shooting" },
-    //     { Id: 4, Name: "Rebounding" },
-    //     { Id: 5, Name: "Transition" },
-    //     { Id: 6, Name: "Defense" },
-    //     { Id: 7, Name: "Team Offense" },
-    //     { Id: 8, Name: "Team Defense" }
-    // ];
-
     var data = [
-       new tagItem("Ball Handling", 1),
-       new tagItem("Passing", 2),
-       new tagItem("Shooting", 3),
-       new tagItem("Rebounding", 4),
-       new tagItem("Transition", 5),
-       new tagItem("Defense", 6),
-       new tagItem("Team Offense", 7),
-       new tagItem("Team Defense", 8)
-   ];
+        { Id: 1, Name: "Ball Handling" },
+        { Id: 2, Name: "Passing" },
+        { Id: 3, Name: "Shooting" },
+        { Id: 4, Name: "Rebounding" },
+        { Id: 5, Name: "Transition" },
+        { Id: 6, Name: "Defense" },
+        { Id: 7, Name: "Team Offense" },
+        { Id: 8, Name: "Team Defense" }
+    ];
+
+//     var data = [
+//        new tagItem("Ball Handling", 1),
+//        new tagItem("Passing", 2),
+//        new tagItem("Shooting", 3),
+//        new tagItem("Rebounding", 4),
+//        new tagItem("Transition", 5),
+//        new tagItem("Defense", 6),
+//        new tagItem("Team Offense", 7),
+//        new tagItem("Team Defense", 8)
+//    ];
 
     function tagItem(name, id){
         return {
-            Name: ko.observable(name),
+            Name: ko.protectedObservable(name),
             Id: ko.observable(id)
         }
     }
 
     var viewModel = {
         //data
-        tags: ko.observableArray(data),
+        tags: ko.observableArray(toTagItemArray(data)),
         tagToAdd: ko.observable(""),
         selectedTag: ko.observable(null),
         //behaviours.
@@ -66,6 +66,7 @@ $(function() {
            buttons: {
                Save: function(){
                    $(this).dialog("close");
+                   viewModel.selectedTag().Name.commit();
                },
                Cancel: function() {
                    $(this).dialog("close");
@@ -77,4 +78,11 @@ $(function() {
 
 
     ko.applyBindings(viewModel);
+
+    function toTagItemArray(data) {
+        var tagItems = ko.utils.arrayMap(data, function (item) {
+           return new tagItem(item.Name, item.Id);
+        });
+        return tagItems;
+    }
 });
